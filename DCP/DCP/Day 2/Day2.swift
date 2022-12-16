9b5b7cad5ae8bb42a98bc39a1818822ef0610841
AVandelay <AVandelay@users.noreply.github.com>
2022-12-16 12:49:20 -0800

    Day 2

---
+++ DCP/DCP/Day 2/Day2.swift
@@ -0,0 +1,37 @@
+
+
+func day2(_ nums: [Int]) -> [Int] {
+    let prefixProducts = nums
+        .reduce(into: [Int]()) { array, value in
+            if let last = array.last {
+                array.append(last * value)
+            } else {
+                array.append(value)
+            }
+        }
+
+    let suffixProducts = Array(nums
+        .reversed()
+        .reduce(into: [Int]()) { array, value in
+            if let last = array.last {
+                array.append(last * value)
+            } else {
+                array.append(value)
+            }
+        }
+        .reversed())
+
+    let result = nums
+        .indices
+        .reduce(into: [Int]()) { array, index in
+            if index == 0 {
+                array.append(suffixProducts[index + 1])
+            } else if index == nums.count - 1 {
+                array.append(prefixProducts[index - 1])
+            } else {
+                array.append(prefixProducts[index - 1] * suffixProducts[index + 1])
+            }
+        }
+
+    return result
+}
--- DCP/DCP/main.swift
+++ DCP/DCP/main.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
-func assert<T: Equatable>(actual: T, expectation: T) {
+func assert<T: Equatable>(_ message: String, _ actual: T, _ expectation: T, function: String = #function) {
     if actual == expectation {
-        print("✅ actual: \(actual) matches expectation: \(expectation)")
+        print("✅ \(function) \(message) actual: \(actual) matches expectation: \(expectation)")
     } else {
-        print("❌ actual: \(actual) does not match expectation: \(expectation)")
+        print("❌ \(function) \(message) actual: \(actual) does not match expectation: \(expectation)")
     }
 }
 
+/// Day 1
+assert("Day 1", day1([10, 15, 3, 7], k: 17), true)
 
+/// Day 2
+assert("Day 2", day2([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
+assert("Day 2", day2([3, 2, 1]), [2, 3, 6])
