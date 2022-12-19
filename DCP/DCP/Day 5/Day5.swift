a13696d9b753164c7712ae73f98963123b137bbd
AVandelay <AVandelay@users.noreply.github.com>
2022-12-19 12:05:48 -0800

    Day 5

---
+++ DCP/DCP/Day 5/Day5.swift
@@ -0,0 +1,26 @@
+
+
+func cons<A, B>(_ a: A, _ b: B) -> (() -> (A, B)) {
+    return { () -> (A, B) in
+        return (a, b)
+    }
+}
+
+func car<A, B>(_ f: () -> (A, B)) -> A {
+    let (a, _) = f()
+    return a
+}
+
+func cdr<A, B>(_ f: () -> (A, B)) -> B {
+    let (_, b) = f()
+    return b
+}
+
+import XCTest
+
+final class Day5: XCTestCase {
+    func test() {
+        XCTAssertEqual(car(cons(3, 4)), 3)
+        XCTAssertEqual(cdr(cons(3, 4)), 4)
+    }
+}
