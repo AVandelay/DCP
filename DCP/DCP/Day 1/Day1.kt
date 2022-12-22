c4f5524fd2432a208bbe829143c36ca6d3168d65
westdorp <34314700+westdorp@users.noreply.github.com>
2022-12-22 11:07:32 -0800

    Day 1

---
+++ DCP/DCP/Day 1/Day1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println(twoSum(listOf(10, 15, 3, 7), 17) == true)
+}
+
+fun twoSum(numbers: List<Int>, k: Int): Boolean  {
+    // Sort the list of numbers
+    val sortedNumbers = numbers.sorted()
+    
+    // Iterate through the list of numbers
+    for (i in sortedNumbers.indices) {
+        // Calculate the difference between k and the current number
+        val difference = k - sortedNumbers[i]
+        
+        // Use binary search to check if the difference is in the list of numbers
+        if (sortedNumbers.binarySearch(difference) >= 0) {
+            return true
+        }
+    }
+    
+    // If we reach this point, it means we have not found any pair that adds up to k
+    return false
+}
---
+++ DCP/DCP/Day 1/Day1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from bisect import bisect_left
+
+def two_sum(numbers, k):
+	sorted_numbers = sorted(numbers)
+	
+	for i in range(len(sorted_numbers)):
+		target = k - sorted_numbers[i]
+		index = bisect_left(sorted_numbers, target)
+		
+		if index < len(sorted_numbers) and sorted_numbers[index] == target:
+			return True
+		
+	return False
+
+print(two_sum([10, 15, 3, 7], 17) == True)
