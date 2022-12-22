94ed8bd517b02bea72d1078de0e82305ed5caa87
westdorp <34314700+westdorp@users.noreply.github.com>
2022-12-22 15:19:09 -0800

    Day 1

--- DCP/DCP/Day 1/Day1.py
+++ DCP/DCP/Day 1/Day1.py
@@ -3,15 +3,15 @@
 from bisect import bisect_left
 
 def two_sum(numbers, k):
-	sorted_numbers = sorted(numbers)
-	
-	for i in range(len(sorted_numbers)):
-		target = k - sorted_numbers[i]
-		index = bisect_left(sorted_numbers, target)
-		
-		if index < len(sorted_numbers) and sorted_numbers[index] == target:
-			return True
-		
-	return False
+    sorted_numbers = sorted(numbers)
+    
+    for i in range(len(sorted_numbers)):
+        target = k - sorted_numbers[i]
+        index = bisect_left(sorted_numbers, target)
+        
+        if index < len(sorted_numbers) and sorted_numbers[index] == target:
+            return True
+        
+    return False
 
 print(two_sum([10, 15, 3, 7], 17) == True)
