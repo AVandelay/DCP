4562d07deba923a9b1f896dd73ee9510a4e08bc9
westdorp <34314700+westdorp@users.noreply.github.com>
2022-12-25 14:06:10 -0800

    Contains Duplicate

---
+++ coding-challenges/sources/leetcode/217. Contains Duplicate/217-ContainsDuplicate.swift
@@ -0,0 +1,15 @@
+
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    nums.count != Set(nums).count
+}
+
+import XCTest
+
+final class ContainsDuplicates: XCTestCase {
+    func test() {
+        XCTAssertTrue(containsDuplicate([1,2,3,1]))
+        XCTAssertFalse(containsDuplicate([1,2,3,4]))
+        XCTAssertTrue(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+    }
+}
